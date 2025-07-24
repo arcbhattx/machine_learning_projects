bb0d7f5d431b4406d0769b2e3077edc245b62099
arcbhattx <arcbh11@gmail.com>
2025-07-23 23:41:56 -0700

    finish later

--- gradient_descent/ex_1/salary_prediction.py
+++ gradient_descent/ex_1/salary_prediction.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataframe = pd.read_csv('archive/Salary_dataset.csv')
+
+print(dataframe.head())
+
+def get_errors(y_true, y_pred):
+    pass;
