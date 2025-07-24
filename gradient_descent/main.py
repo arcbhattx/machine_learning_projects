5533d0015ec3d138ce6cf6bb724144d4946c363d
arcbhattx <arcbh11@gmail.com>
2025-07-23 18:52:34 -0700

    finished function

--- gradient_descent/main.py
+++ gradient_descent/main.py
@@ -32,3 +32,33 @@ def gradient_descent(x,y,iterations = 1000, learning_rate=0.0001, stopping_thres
 
         if previous_cost and abs(previous_cost-current_cost) <= stopping_threshold:
             break;
+
+        previous_cost = current_cost
+
+        costs.append(current_cost)
+        weights.append(current_weight)
+
+        
+        #calculation gradients:
+        weight_derivative = -(2/n) * sum(x * (y-y_predicted))
+        bias_derivative = -(2/n) * sum(y-y_predicted)
+        
+
+        #updating values:
+        current_weight = current_weight - (learning_rate * weight_derivative)
+        current_bias = current_bias - (learning_rate * bias_derivative)
+
+        #print for each  iteration:
+        print(f"Iteration {i+1}: Cost {current_cost}, Weight \
+              {current_weight}, Bias {current_bias}")
+        
+
+        plt.figure(figsize=(8,6))
+        plt.plot(weights, costs)
+        plt.scatter(weights, costs, marker='o', color='red')
+        plt.title("Costs vs Weights")
+        plt.ylabel("Cost")
+        plt.xlabel("Weight")
+        plt.show()
+
+        return current_weight,current_bias
